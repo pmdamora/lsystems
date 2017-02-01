5347fc0d1def8fa5c844d0d4a4720d15737236cc
Paul D'Amora <pmdamora@gmail.com>
2017-02-01 18:09:28 -0500

    Added galaxy lsystem

--- LsysExpand.m
+++ LsysExpand.m
@@ -6,8 +6,9 @@
 
 function [lsys] = LsysExpand(rules, axiom, nReps)
     lsys = axiom;
+    cellRules = struct2cell(rules');
     for i=1:nReps
         % One-liner to perform a regex replacement on the entire string
-        lsys = regexprep(lsys,rules.before,rules.after);
+        lsys = regexprep(lsys,cellRules(1,:),cellRules(2,:));
     end
 end
